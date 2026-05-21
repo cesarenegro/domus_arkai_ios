1d1203d08b1d2fd40c49eca7251120fb1d34db0d
cesarenegro <cesare.negro@gmail.com>
2026-05-22 07:03:13 +0800

    Initial Commit

---
+++ Domus Arkai/Domus_ArkaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Domus_ArkaiApp.swift
+//  Domus Arkai
+//
+//  Created by Cesare on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Domus_ArkaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
